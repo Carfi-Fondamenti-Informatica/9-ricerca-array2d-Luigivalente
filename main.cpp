f87f43eb85c34b8cfbbfaacfbff26e0ae44d9b8b
Luigivalente <91776312+Luigivalente@users.noreply.github.com>
2021-12-11 16:38:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char matnomi[r][c];
+    char nome[c];
+    int indice;
+    for(int i=0; i<r; i++){
+        cin >>matnomi[i];
+    }
+    cin >>nome;
+    if(contenuto(matnomi, nome,indice)==true){
+        cout<< indice <<endl;
+    }else{
+        cout<< "non presente"<<endl;
+    }
+    return 0;
 }
